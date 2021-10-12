ab7fc476a25d86cb9b388f1a23807b65fc9124a7
surajgp65 <surajgp65@gmail.com>
2021-10-12 13:16:42 +0530

    Added some code

--- Assets/Js/custom.js
+++ Assets/Js/custom.js
@@ -11,8 +11,24 @@ $(document).ready(function() {
 
     // Toggle menu/navbar script
 
-    $('.menu-btn').click(function() {
+    $('.navbar-toggler').click(function() {
         $('.navbar_ai .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
     });
+});
+
+jQuery(document).ready(function($) {
+    var alterClass = function() {
+        var ww = document.body.clientWidth;
+        if (ww < 600) {
+            $('.navbar_ai').addClass('bg-dark');
+        } else if (ww >= 601) {
+            $('.navbar_ai').removeClass('bg-dark');
+        };
+    };
+    $(window).resize(function() {
+        alterClass();
+    });
+    //Fire it when the page first loads:
+    alterClass();
 });
